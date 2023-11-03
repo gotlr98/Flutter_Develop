9d920351560beefec83f60ad88086001042636f6
gotlr98 <gotlr98@naver.com>
2023-11-03 17:11:17 +0900

    feat: add .gitignore - *.env & add API KEY into .env

--- temp/lib/main.dart
+++ temp/lib/main.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  final request_url = 'https://openapi.naver.com/v1/search/encyc.json';
-
   @override
   State<MyApp> createState() => _MyAppState();
+
+  Future<void> searchNaver() async {
+    const requestUrl = 'https://openapi.naver.com/v1/search/encyc.json';
+
+    try {
+      http.Response response =
+          await http.get(Uri.parse('$requestUrl?query=테스트'), headers: {
+        'X-Naver-Client-Id': dotenv.env['Client_ID']!,
+        'X-Naver-Client-Secret': dotenv.env['Client_Secret']!,
+      });
+      if (response.statusCode == 200) {
+        print('error');
+      }
+    } catch (error) {}
+  }
 }
 
 class _MyAppState extends State<MyApp> {
